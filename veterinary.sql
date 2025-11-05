3f7c20cc9d097a3a25692be255e6d84e7a0b0e71
jamesandrei1234 <jamesandreialvez@gmail.com>
2025-11-05 22:12:52 +0800

    feat/insert-10-rows-medicalrecords

--- veterinary.sql
+++ veterinary.sql
@@ -46,13 +46,15 @@ CREATE TABLE invoices (
 CREATE TABLE medicalrecords (
     recordid INT PRIMARY KEY,
     animalid INT,
+    recorddate DATETIME,
     doctorid INT,
     diagnosis VARCHAR(250),
-    treatment TEXT,
-    recorddate DATE,
+    prescription VARCHAR(100),
+    notes TEXT,
     FOREIGN KEY (animalid) REFERENCES animals(animalid),
     FOREIGN KEY (doctorid) REFERENCES doctors(doctorid)
 );
+
 INSERT INTO owners (ownerid, ofirstname, olastname, address, phone, email) VALUES
 (1, 'Juan', 'Dela Cruz', '123 Narra St, Lipa City', '09171234567', 'juan.dc@example.com'),
 (2, 'Ben', 'Reyes', '456 Manga Ave, Batangas City', '09181234567', 'ben.reyes@example.com'),
@@ -108,3 +110,15 @@ INSERT INTO invoices (invoiceid, appointid, amount, paymenttime) VALUES
 (8, 8, 150.00, '16:30:00'),
 (9, 9, 60.00, '14:45:00'),
 (10, 10, 40.00, '11:30:00');
+INSERT INTO medicalrecords (recordid, animalid, recorddate, doctorid, diagnosis, prescription, notes)
+VALUES 
+    (1, 1, '2023-01-05 00:00:00', 1, 'Health check', 'N/A', 'Regular checkup, no issue detected'),
+    (2, 2, '2023-01-10 00:00:00', 1, 'Vaccination', 'Vaccine X', 'Administered vaccination X as per schedule'),
+    (3, 3, '2023-02-02 00:00:00', 3, 'Sprained leg', 'Pain Medication', 'Rest recommended for two weeks'),
+    (4, 4, '2023-02-15 00:00:00', 1, 'Dental cleaning', 'N/A', 'Completed dental cleaning procedure'),
+    (5, 5, '2023-03-10 00:00:00', 4, 'Skin infection', 'Antibiotics', 'Prescribed antibiotics for skin infection'),
+    (6, 6, '2023-03-10 00:00:00', 2, 'Flea infestation', 'Flea Treatment', 'Administered flea treatment'),
+    (7, 7, '2023-04-12 00:00:00', 1, 'Vaccination', 'Vaccine Y', 'Administered vaccination Y as per schedule'),
+    (8, 8, '2023-04-18 00:00:00', 5, 'Spaying', 'N/A', 'Successfully performed spaying surgery'),
+    (9, 9, '2023-05-02 00:00:00', 4, 'Allergic reaction', 'Antihistamines', 'Allergic reaction due to food prescribed antihistamine'),
+    (10, 10, '2023-05-20 00:00:00', 6, 'Conjunctivitis', 'Eye drops', 'Prescribed eye drops for conjunctivitis');
