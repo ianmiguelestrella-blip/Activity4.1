2aaeb35d3b27acd7cb37f6d52c32f811f0b00acb
jamesandrei1234 <jamesandreialvez@gmail.com>
2025-11-05 22:24:27 +0800

    fix/rename-column-paymenttime

--- veterinary.sql
+++ veterinary.sql
@@ -124,3 +124,7 @@ VALUES
     (10, 10, '2023-05-20 00:00:00', 6, 'Conjunctivitis', 'Eye drops', 'Prescribed eye drops for conjunctivitis');
 ALTER TABLE owners
 ADD COLUMN registereddate DATE;
+
+ALTER TABLE invoices
+RENAME COLUMN paymentdate To paymenttime;
+
